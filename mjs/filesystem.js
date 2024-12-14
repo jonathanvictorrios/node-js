95d783d3cf29a6dd48a92be57bf056d6b8945ddb
jonavictorrios <jonathan.rios-ls@alitaware.com>
2024-12-14 13:56:19 -0300

    all comments are deleted

--- mjs/filesystem.js
+++ mjs/filesystem.js
@@ -3,8 +3,8 @@ const fs = require('node:fs') //a partir de node 16 se recomienda poner node:
 const stats = fs.statSync(__dirname + '/archivo.txt') 
 console.log(
     
-    "es un archivo?:"+stats.isFile(),
-    "es un Directorio?:"+stats.isDirectory(),
+    stats.isFile(),
+    stats.isDirectory(),
     stats.isSymbolicLink(),
     stats.size,
 );
